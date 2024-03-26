ec278814204e40e6ecc619d2183db5edfd4095b2
MetaphorC <ypstomar57@gmail.com>
2024-03-27 00:29:17 +0530

    Done initial setup

---
+++ lua/core/mappings.lua
@@ -0,0 +1,5 @@
+-- Vim keybindings
+vim.g.mapleader = " " --easy to reach leader key, which is <space>, of course
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+
+
---
+++ lua/core/plugins.lua
@@ -0,0 +1,74 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+
+use({
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+		vim.cmd('colorscheme rose-pine')
+	end
+})
+
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('nvim-treesitter/playground')
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
+use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+}
+
+-- lsp-zero
+use {
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v3.x',
+	requires = {
+		--- Uncomment the two plugins below if you want to manage the language servers from neovim
+		-- {'williamboman/mason.nvim'},
+		-- {'williamboman/mason-lspconfig.nvim'},
+
+		-- LSP Support
+		{'neovim/nvim-lspconfig'},
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'L3MON4D3/LuaSnip'},
+	}
+}
+
+use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+}
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+	  require('packer').sync()
+  end
+end)
+
+	
